60f42d5a425d1d50da57c63791bb008690e022f4
Aliaxander <admin@oxcpa.ru>
2017-09-16 21:57:17 +0300

    parseattachmets

--- commands/ParseController.php
+++ commands/ParseController.php
@@ -29,12 +29,17 @@ class ParseController extends Controller
     public function actionIndex($message = 'hello world')
     {
         echo $message . "\n";
+//        $vk = new \VK\VK('6180749 ', 'QrNebYK25HTxXwrvWW5g');
+//        $vk->setAccessToken('512ac84d512ac84d512ac84d26517487c05512a512ac84d089c90d1471a0245796ec90e');
+//        $result = $vk->api('wall.get', ['owner_id' => '-14897324', 'count' => 1]);
+//        print_r($result);
+//        die;
         $projects = Projects::find()->all();
         foreach ($projects as $project) {
             $vk = new \VK\VK('6180749 ', 'QrNebYK25HTxXwrvWW5g');
             $vk->setAccessToken('512ac84d512ac84d512ac84d26517487c05512a512ac84d089c90d1471a0245796ec90e');
-            $result = $vk->api('wall.get', ['owner_id' => $project->vkId, 'count' => 1000]);
-            
+            $result = $vk->api('wall.get', ['owner_id' => $project->vkId, 'count' => 100]);
+            //14897324
             foreach ($result['response'] as $row) {
                 if (is_array($row)) {
                     $row = (object)$row;
@@ -44,7 +49,7 @@ class ParseController extends Controller
                         $news->text = $row->text;
                         $news->token = $row->id . "_" . $project->vkId;
                         $news->media = @json_encode(@$row->media);
-                        $news->attachment = @json_encode(@$row->attachment);
+                        $news->attachment = @json_encode(@$row->attachments);
                         $news->project = $project->id;
                         var_dump($news->save());
                     } catch (\Exception $e) {
